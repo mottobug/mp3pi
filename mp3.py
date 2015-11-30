99b568a0b3071a49241d9878019b962d9d5648be
Thomas Koch <tom@harhar.net>
2015-11-30 16:43:09 +0100

    ...

--- mp3.py
+++ mp3.py
@@ -16,9 +16,13 @@ import alsaaudio
 import sys
 import json
 import pprint
+import requests
 from signal import SIGTSTP, SIGTERM, SIGABRT
 
 ##from threading import Thread
+
+user_agent = {'User-agent': 'User-Agent: XBMC Addon Radio'}
+
 
 def set_mixer(name, args, kwargs):
     try:
@@ -44,11 +48,44 @@ def get_mixer(name, kwargs):
 
 listitems = []
 def get_Stations():
+  json_data = []
   with open('json.json') as data_file:    
-    data = json.load(data_file)
+    json_data = json.load(data_file)
 
-  for item in data:
+  for item in json_data:
     listitems.append(item['name'])
+
+def get_Station(name):
+  json_data = []
+  id = 0
+  with open('json.json') as data_file:    
+    json_data = json.load(data_file)
+
+  for item in json_data:
+    if item['name'] == name:
+      id = item['id']
+
+  #
+  url = "http://radio.de/info/broadcast/getbroadcastembedded?broadcast=" + str(id)
+
+  response = requests.get(url, headers = user_agent)
+#  print(response.status_code)
+  data = response.json()
+
+  if "errorCode" in data.keys():
+    print("no such entry")
+    return(0)
+  
+  print(data['link'])
+  print(data['name'])
+  print(data['streamURL'])
+
+  return(data['streamURL'])
+#  if "StreamURLs" in data.keys():
+#    for item in data['streamURLs']:
+#      print(item['streamURL'])
+
+
 
 
 class Mp3PiAppLayout(BoxLayout):
@@ -57,10 +94,18 @@ class Mp3PiAppLayout(BoxLayout):
   isPlaying = False
   proc = None
 
+
+#  def args_converter(self, row_index, an_obj):
+#    
+#    if an_obj is "0":
+#      text = "HR3"
+#    if an_obj is "1":
+#      text = "FFH"
+#    return {'text': text,
+#            'size_hint_y': None,
+#            'height': 25}
+
   def __init__(self, **kwargs):
-
-		
-
     super(Mp3PiAppLayout, self).__init__(**kwargs)
     #self.search_results.adapter.data.extend(("HR-Info", "HR3", "Radio Bob"))
     self.search_results.adapter.data.extend((listitems))
@@ -72,9 +117,12 @@ class Mp3PiAppLayout(BoxLayout):
     set_mixer("Master", int(args), {})
 
   def change_selection(self, args):
+    get_Stations()
     if args.selection:
       print(args.selection[0].text)
-      self.change_image(args.selection[0].text)
+      print(get_Station(args.selection[0].text))
+      self.change_image("url", get_Station(args.selection[0].text))
+      #self.change_image(args.selection[0].text)
 
   def start_second_thread(self, l_text):
     if self.isPlaying == 0:
@@ -88,21 +136,10 @@ class Mp3PiAppLayout(BoxLayout):
         self.proc = None
         self.stop.set()
       
-  def infinite_loop(self, label):
+  def infinite_loop(self, url):
     iteration = 0
     
-    url = "http://mp3channels.webradio.antenne.de/chillout"
-    
-    if label == "Radio Bob":
-      url = "http://streams.radiobob.de/bob-live/mp3-192/mediaplayerbob"
-
-    if label == "HR3":
-      url = "http://hr-mp3-m-h3.akacast.akamaistream.net/7/785/142133/v1/gnl.akacast.akamaistream.net/hr-mp3-m-h3"
-    
-    if label == "HR-Info":
-      url = "http://hr-mp3-m-hrinfo.akacast.akamaistream.net/7/698/142135/v1/gnl.akacast.akamaistream.net/hr-mp3-m-hrinfo"
-    
-    self.proc = subprocess.Popen(["mpg123", "-o", "pulse", url])
+    self.proc = subprocess.Popen(["mpg123", "-o", "pulse", "-@", url])
 
     while True:
       if self.stop.is_set():
@@ -113,18 +150,18 @@ class Mp3PiAppLayout(BoxLayout):
       print(self.isPlaying)
       time.sleep(1)
 
-  def change_image(self, args):
-    print(args)
+  def change_image(self, image, url):
+
     print(self.ids.imageid.source)
 
-    if args == "Radio Bob":
-      self.ids.imageid.source = "bob.jpg"
-    if args == "HR3":
-      self.ids.imageid.source = "hr3.jpg"
-    if args == "hr-info":
-      self.ids.imageid.source = "hr-info.png"
+#    if args == "Radio Bob":
+#      self.ids.imageid.source = "bob.jpg"
+#    if args == "HR3":
+#      self.ids.imageid.source = "hr3.jpg"
+#    if args == "hr-info":
+#      self.ids.imageid.source = "hr-info.png"
 
-    self.start_second_thread(args)
+    self.start_second_thread(url)
     pass
 
 class Mp3PiApp(App):
